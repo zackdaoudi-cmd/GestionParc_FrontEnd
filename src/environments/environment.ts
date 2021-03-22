6eeeb2d5e9ffc6e30dd069176e0d2d8625de38f0
Daoudi Zakaria <62399266+zackdaoudi-cmd@users.noreply.github.com>
2021-03-22 18:40:40 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
 };
 
 export const baseUrl  = `http://localhost:8080/`;
-/*
+export  const token = localStorage.getItem('token');
+export const urlInter =`http://localhost:8000/`;/*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
